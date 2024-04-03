59b421d17e0526945ba62a9abdc24d251f2cfc83
Joshua Gorospe <joshua.gorospe@gmail.com>
2024-04-03 21:25:19 +0000

    Add current status

--- Lesson4CodeSessions_FundMe.sol
+++ Lesson4CodeSessions_FundMe.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+/*
 contract FundMe {
    
+    uint256 public minimumUSD = 50
+
     function fund() public payable{
         // Goal is to send ETH to this contract
         // This will set a minimum USD amount, and give an error message when amount is to low
@@ -13,5 +17,47 @@ contract FundMe {
 
 
 }
+*/
 
 
+contract FundMe {
+   
+    uint256 public minimumUSD = 50 * 1e18;
+
+    function fund() public payable{
+
+        require(msg.value >= minimumUSD, "You did not send enough funds in USD."); 
+
+    }
+
+    // Note to self: Chainlink is capable of sending and receiving API calls. 
+    // This is powerful and can be combined with Chainlink Data Feeds, 
+    // Chainlink VRF (Random Number Generators, and Chainlink Automation (automated Upkeep/Triggering/Polling).
+    function getPrice() public view returns(uint256) {
+        // Need 2 things...
+        // 1. ABI - for this getPrice function we need to create an interface
+        // 2. Address
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43);
+        (,int price,,,) = priceFeed.latestRoundData();
+        // ETH in USD
+        // Roughly 3000.00000000
+        return uint256(price * 1e10);
+    }
+     
+    function getVersion() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43);
+        return priceFeed.version();
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+
+        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
+
+        return ethAmountInUSD;
+
+    }
+
+    //function withdraw(){ }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 These are my notes while working on this Solidity course section.
 
+- On April 3, 2024 successfully compiled and deployed the following "fun and getPrice etc." example code. 
+  - Note to self: I left off at [4:25 in Patrick's YouTube course video](https://youtu.be/gyMwXuJrbJQ?t=15902).
+![alt text](image1.png)
