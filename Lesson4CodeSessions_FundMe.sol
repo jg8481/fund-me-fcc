6a8a59f4f6dc9e48db5232ad7d612f5034a8fd0e
Joshua Gorospe <joshua.gorospe@gmail.com>
2024-04-03 23:20:58 -0400

    Update Lesson4CodeSessions_FundMe.sol

--- Lesson4CodeSessions_FundMe.sol
+++ Lesson4CodeSessions_FundMe.sol
@@ -10,7 +10,7 @@ contract FundMe {
 
     function fund() public payable{
         // Goal is to send ETH to this contract
-        // This will set a minimum USD amount, and give an error message when amount is to low
+        // This will set a minimum USD amount, and give an error message when amount is too low
         require(msg.value > 1e18, "You did not send enough ETH."); // 1e18 = 1 ETH in Wei
         // ^ This "require" statement will output an error and revert (or undo the transaction) if the condition is not met
     }
